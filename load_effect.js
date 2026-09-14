a0a9cb4d95f454387b70913c40bf3d19733cb700
macdonald <macdonaldbarasa2026@gmail.com>
2026-09-14 18:58:22 +0300

    chore: remove obsolete maintenance scripts

--- load_effect.js
+++
@@ -1,8 +0,0 @@
-  useEffect(() => {
-    loadRecentConversations().then((conversations) => {
-      if (conversations && conversations.length > 0) {
-        // Just load the most recent conversation's messages for context
-        setChatHistory(conversations[0].messages || []);
-      }
-    });
-  }, []);
